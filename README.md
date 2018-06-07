f846ee6c90284aa1c81f19659634b881364cd990
Vladimir Stratiev <vladimir_stratiev@yahoo.com>
2018-06-07 11:09:18 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PlurlTweet
-Plurlsight Tweeter Messanger
+
+**TODO: Add description**
 
 ## Installation
 
@@ -12,8 +13,9 @@ def deps do
     {:plurl_tweet, "~> 0.1.0"}
   ]
 end
-
 ```
+
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/plurl_tweet].
+
---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule PlurlTweet.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :plurl_tweet,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :quantum, :extwitter],
+      mod: {PlurlTweet.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 0.9.3"},
+      {:quantum, "~> 2.2"},
+      {:extwitter, "~> 0.9.3"},
+      {:oauth, github: "tim/erlang-oauth"},
+      {:mock, "~> 0.3.1"},
+      {:mix_test_watch, "~> 0.6.0"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+end
